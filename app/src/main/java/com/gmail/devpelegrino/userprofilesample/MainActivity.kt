d5cfd80fbb47594819d42846c675255c8651932d
Felipe Pelegrino <felipe.pelegrino99@gmail.com>
2024-03-26 11:51:23 -0300

    feat: set window edgeToEdge and window system status bar transparent

--- app/src/main/java/com/gmail/devpelegrino/userprofilesample/MainActivity.kt
+++ app/src/main/java/com/gmail/devpelegrino/userprofilesample/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.gmail.devpelegrino.userprofilesample
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gmail.devpelegrino.userprofilesample.ui.theme.UserProfileSampleTheme
 import kotlin.random.Random
@@ -20,15 +26,39 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContent {
+            val detectDarkMode = true
+            // Turn off the decor fitting system windows, which allows us to handle insets,
+            // including IME animations, and go edge-to-edge
+            // This also sets up the initial system bar style based on the platform theme
+            enableEdgeToEdge()
+
+            DisposableEffect(detectDarkMode) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        Color.TRANSPARENT,
+                        Color.TRANSPARENT,
+                    ) { detectDarkMode },
+                    navigationBarStyle = SystemBarStyle.auto(
+                        lightScrim,
+                        darkScrim,
+                    ) { detectDarkMode },
+                )
+                onDispose {}
+            }
+
             UserProfileSampleTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(name = LoremIpsum(
-                        Random.nextInt(1..100)
-                    ).values.joinToString())
+                    Greeting(
+                        name = LoremIpsum(
+                            Random.nextInt(1..100)
+                        ).values.joinToString(),
+                        Modifier.padding(top = 48.dp)
+                    )
                 }
             }
         }
@@ -50,3 +80,15 @@ fun GreetingPreview() {
 
     }
 }
+
+/**
+ * The default light scrim, as defined by androidx and the platform:
+ * https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:activity/activity/src/main/java/androidx/activity/EdgeToEdge.kt;l=35-38;drc=27e7d52e8604a080133e8b842db10c89b4482598
+ */
+private val lightScrim = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+
+/**
+ * The default dark scrim, as defined by androidx and the platform:
+ * https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:activity/activity/src/main/java/androidx/activity/EdgeToEdge.kt;l=40-44;drc=27e7d52e8604a080133e8b842db10c89b4482598
+ */
+private val darkScrim = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b)
--- app/src/main/java/com/gmail/devpelegrino/userprofilesample/ui/theme/Theme.kt
+++ app/src/main/java/com/gmail/devpelegrino/userprofilesample/ui/theme/Theme.kt
@@ -1,15 +1,9 @@
 package com.gmail.devpelegrino.userprofilesample.ui.theme
 
-import android.app.Activity
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -78,18 +72,9 @@ private val DarkColors = darkColorScheme(
 
 @Composable
 fun UserProfileSampleTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val colorScheme = DarkColors
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
