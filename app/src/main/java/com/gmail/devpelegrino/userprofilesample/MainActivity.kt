f66d08351e189283a6f6aa5de1e0754f358361b4
Felipe Pelegrino <felipe.pelegrino99@gmail.com>
2024-03-26 11:21:41 -0300

    feat: add splash screen core | set night themes

--- app/src/main/java/com/gmail/devpelegrino/userprofilesample/MainActivity.kt
+++ app/src/main/java/com/gmail/devpelegrino/userprofilesample/MainActivity.kt
@@ -10,10 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gmail.devpelegrino.userprofilesample.ui.theme.UserProfileSampleTheme
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             UserProfileSampleTheme {
@@ -21,7 +26,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Greeting(name = LoremIpsum(
+                        Random.nextInt(1..100)
+                    ).values.joinToString())
                 }
             }
         }
