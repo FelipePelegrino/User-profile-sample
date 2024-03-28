4ba920a0b7ff345955bcd68ab2df80c245d0e261
Felipe Pelegrino <felipe.pelegrino99@gmail.com>
2024-03-27 21:27:12 -0300

    feat: iniciando UserProfileAppState

---
+++ app/src/main/java/com/gmail/devpelegrino/userprofilesample/ui/UserProfileAppState.kt
@@ -0,0 +1,28 @@
+package com.gmail.devpelegrino.userprofilesample.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Stable
+class UserProfileAppState(
+//    val navController: NavHostController
+) {
+//    val currentDestination: NavDestination?
+//        @Composable get() = navController.currentBackStackEntryAsState().value?.destination
+
+    /*
+    * TODO: definir rotas do app
+    *   • Profile
+    *   • Edit
+    *   • Profile -> Edit
+    *
+    * TopLevelDestionation: Profile
+    * */
+
+   init {
+
+   }
+}
